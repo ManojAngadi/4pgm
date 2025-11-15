492e5f298501a657c0ee1b70ec5736c03e6881fb
Manoj Angadi <123240720+ManojAngadi@users.noreply.github.com>
2025-11-15 09:08:09 +0530

    Create arth_oper.py

---
+++ arth_oper.py
@@ -0,0 +1,10 @@
+try:
+  a=float(input("enter first number"))
+  b=float(input("enter second number"))
+
+  print(f"Addition:{a+b}")
+  print(f"Subtraction:{a-b}")
+expect ValueError:
+print("please enter valid numbers.")
+
+
